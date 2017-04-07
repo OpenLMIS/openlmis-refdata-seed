fed9fa57bd2fd49af84e82a5b7ecf7bf8a29257d
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-04-07 13:22:51 +0200

    Added PMD plugin

--- src/main/java/mw/gov/health/lmis/upload/Services.java
+++ src/main/java/mw/gov/health/lmis/upload/Services.java
@@ -1,6 +1,7 @@
 package mw.gov.health.lmis.upload;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import mw.gov.health.lmis.utils.SourceFile;
@@ -9,37 +10,7 @@ import mw.gov.health.lmis.utils.SourceFile;
 public class Services {
 
   @Autowired
-  private ProgramService programService;
-
-  @Autowired
-  private StockAdjustmentReasonService stockAdjustmentReasonService;
-
-  @Autowired
-  private GeographicLevelService geographicLevelService;
-
-  @Autowired
-  private GeographicZoneService geographicZoneService;
-
-  @Autowired
-  private RoleService roleService;
-
-  @Autowired
-  private RightService rightService;
-
-  @Autowired
-  private FacilityOperatorService facilityOperatorService;
-
-  @Autowired
-  private FacilityTypeService facilityTypeService;
-
-  @Autowired
-  private ProcessingScheduleService processingScheduleService;
-
-  @Autowired
-  private ProcessingPeriodService processingPeriodService;
-
-  @Autowired
-  private SupervisoryNodeService supervisoryNodeService;
+  private ApplicationContext applicationContext;
 
   /**
    * Retrieves service by the human readable name.
@@ -48,40 +19,11 @@ public class Services {
    * @return the corresponding service
    */
   public BaseCommunicationService getService(String name) {
-    switch (name) {
-      case "Program":
-        return programService;
-      case "StockAdjustmentReason":
-      case "Stock Adjustment Reason":
-        return stockAdjustmentReasonService;
-      case "Geographic Zone":
-      case "GeographicZone":
-        return geographicZoneService;
-      case "Geographic Level":
-      case "GeographicLevel":
-        return geographicLevelService;
-      case "Facility Type":
-      case "FacilityType":
-        return facilityTypeService;
-      case "Facility Operator":
-      case "FacilityOperator":
-        return facilityOperatorService;
-      case "Role":
-        return roleService;
-      case "Right":
-        return rightService;
-      case "Processing Schedule":
-      case "ProcessingSchedule":
-        return processingScheduleService;
-      case "Processing Period":
-      case "ProcessingPeriod":
-        return processingPeriodService;
-      case "Supervisory Node":
-      case "SupervisoryNode":
-        return supervisoryNodeService;
-      default:
-        return null;
-    }
+    String safeName = name.replace(" ", "");
+    String serviceName = String.format(
+        "%s%sService", Character.toLowerCase(safeName.charAt(0)), safeName.substring(1)
+    );
+    return applicationContext.getBean(serviceName, BaseCommunicationService.class);
   }
 
   /**
@@ -91,29 +33,9 @@ public class Services {
    * @return the corresponding service
    */
   public BaseCommunicationService getService(SourceFile source) {
-    switch (source) {
-      case PROGRAMS:
-        return programService;
-      case STOCK_ADJUSTMENT_REASONS:
-        return stockAdjustmentReasonService;
-      case GEOGRAPHIC_LEVELS:
-        return geographicLevelService;
-      case GEOGRAPHIC_ZONES:
-        return geographicZoneService;
-      case ROLES:
-        return roleService;
-      case FACILITY_TYPES:
-        return facilityTypeService;
-      case FACILITY_OPERATORS:
-        return facilityOperatorService;
-      case PROCESSING_SCHEDULE:
-        return processingScheduleService;
-      case PROCESSING_PERIOD:
-        return processingPeriodService;
-      case SUPERVISORY_NODES:
-        return supervisoryNodeService;
-      default:
-        return null;
-    }
+    int nameLength = source.getName().length();
+    // remove plural
+    String safeName = source.getName().substring(0, nameLength - 1);
+    return getService(safeName);
   }
 }
---
+++ src/test/java/mw/gov/health/lmis/upload/ServicesTest.java
@@ -0,0 +1,124 @@
+package mw.gov.health.lmis.upload;
+
+import static mw.gov.health.lmis.utils.SourceFile.FACILITY_OPERATORS;
+import static mw.gov.health.lmis.utils.SourceFile.FACILITY_TYPES;
+import static mw.gov.health.lmis.utils.SourceFile.GEOGRAPHIC_LEVELS;
+import static mw.gov.health.lmis.utils.SourceFile.GEOGRAPHIC_ZONES;
+import static mw.gov.health.lmis.utils.SourceFile.PROCESSING_PERIOD;
+import static mw.gov.health.lmis.utils.SourceFile.PROCESSING_SCHEDULE;
+import static mw.gov.health.lmis.utils.SourceFile.PROGRAMS;
+import static mw.gov.health.lmis.utils.SourceFile.ROLES;
+import static mw.gov.health.lmis.utils.SourceFile.STOCK_ADJUSTMENT_REASONS;
+import static mw.gov.health.lmis.utils.SourceFile.SUPERVISORY_NODES;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import mw.gov.health.lmis.utils.SourceFile;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServicesTest.Config.class)
+public class ServicesTest {
+  private static final Map<SourceFile, Class> MAP;
+
+  static {
+    MAP = ImmutableMap
+        .<SourceFile, Class>builder()
+        .put(PROGRAMS, ProgramService.class)
+        .put(STOCK_ADJUSTMENT_REASONS, StockAdjustmentReasonService.class)
+        .put(GEOGRAPHIC_LEVELS, GeographicLevelService.class)
+        .put(GEOGRAPHIC_ZONES, GeographicZoneService.class)
+        .put(ROLES, RoleService.class)
+        .put(FACILITY_TYPES, FacilityTypeService.class)
+        .put(FACILITY_OPERATORS, FacilityOperatorService.class)
+        .put(PROCESSING_SCHEDULE, ProcessingScheduleService.class)
+        .put(PROCESSING_PERIOD, ProcessingPeriodService.class)
+        .put(SUPERVISORY_NODES, SupervisoryNodeService.class)
+        .build();
+  }
+
+  @Configuration
+  @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CommandLineRunner.class))
+  @EnableAutoConfiguration
+  static class Config {
+
+    @Bean
+    public mw.gov.health.lmis.Configuration configuration() {
+      return new mw.gov.health.lmis.Configuration();
+    }
+
+  }
+
+  @Autowired
+  private Services services;
+
+  @Test
+  public void shouldFindServiceBySourceFile() throws Exception {
+    Arrays
+        .stream(SourceFile.values())
+        .forEach(source -> {
+          assertServiceType(source, MAP.get(source));
+        });
+  }
+
+  @Test
+  public void shouldFindServiceByName() throws Exception {
+    assertServiceType("Program", ProgramService.class);
+
+    assertServiceType("StockAdjustmentReason", StockAdjustmentReasonService.class);
+    assertServiceType("Stock Adjustment Reason", StockAdjustmentReasonService.class);
+
+    assertServiceType("Geographic Zone", GeographicZoneService.class);
+    assertServiceType("GeographicZone", GeographicZoneService.class);
+
+    assertServiceType("Geographic Level", GeographicLevelService.class);
+    assertServiceType("GeographicLevel", GeographicLevelService.class);
+
+    assertServiceType("Facility Type", FacilityTypeService.class);
+    assertServiceType("FacilityType", FacilityTypeService.class);
+
+    assertServiceType("Facility Operator", FacilityOperatorService.class);
+    assertServiceType("FacilityOperator", FacilityOperatorService.class);
+
+    assertServiceType("Role", RoleService.class);
+
+    assertServiceType("Right", RightService.class);
+
+    assertServiceType("Processing Schedule", ProcessingScheduleService.class);
+    assertServiceType("ProcessingSchedule", ProcessingScheduleService.class);
+
+    assertServiceType("Processing Period", ProcessingPeriodService.class);
+    assertServiceType("ProcessingPeriod", ProcessingPeriodService.class);
+
+    assertServiceType("Supervisory Node", SupervisoryNodeService.class);
+    assertServiceType("SupervisoryNode", SupervisoryNodeService.class);
+  }
+
+  private void assertServiceType(String name, Class clazz) {
+    assertThat(services.getService(name), instanceOf(clazz));
+  }
+
+  private void assertServiceType(SourceFile source, Class clazz) {
+    assertThat("Add entry in static block to fix this assert", clazz, is(notNullValue()));
+    assertThat(services.getService(source), instanceOf(clazz));
+  }
+
+}
