4f15e4424e1f4b642f5d8b22fd372e9eb11b4fa6
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-04-11 13:58:32 +0200

    Improved CommodityTypeService

--- src/main/java/mw/gov/health/lmis/upload/CommodityTypeService.java
+++ src/main/java/mw/gov/health/lmis/upload/CommodityTypeService.java
@@ -1,13 +1,7 @@
 package mw.gov.health.lmis.upload;
 
-import static mw.gov.health.lmis.upload.RequestHelper.createUri;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestClientResponseException;
 
 @Service
 public class CommodityTypeService extends BaseCommunicationService {
@@ -18,28 +12,8 @@ public class CommodityTypeService extends BaseCommunicationService {
   }
 
   @Override
-  public boolean createResource(String json) {
-    String url = configuration.getHost() + getUrl();
-
-    RequestParameters parameters = RequestParameters
-        .init()
-        .set(ACCESS_TOKEN, authService.obtainAccessToken());
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    HttpEntity<String> body = new HttpEntity<>(json, headers);
-
-    try {
-      restTemplate.put(createUri(url, parameters), body);
-    } catch (RestClientResponseException ex) {
-      logger.error("Can not create resource: {}", ex.getResponseBodyAsString());
-      return false;
-    } catch (RestClientException ex) {
-      logger.error("Can not create resource: {}", ex.getMessage());
-      return false;
-    }
-
-    return true;
+  public HttpMethod getCreateMethod() {
+    return HttpMethod.PUT;
   }
 
 }
