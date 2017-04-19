d5627d85bea75cac0f77091c1b718e52d3581d7e
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2017-04-19 19:49:01 +0200

    Fix supply line search

--- src/main/java/mw/gov/health/lmis/upload/SupplyLineService.java
+++ src/main/java/mw/gov/health/lmis/upload/SupplyLineService.java
@@ -2,7 +2,9 @@ package mw.gov.health.lmis.upload;
 
 import org.springframework.stereotype.Service;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonString;
 
 @Service
 public class SupplyLineService extends BaseCommunicationService {
@@ -14,7 +16,24 @@ public class SupplyLineService extends BaseCommunicationService {
 
   @Override
   public JsonObject findUnique(JsonObject object) {
-    return findByCode(object.getString(CODE));
+    JsonString searchedSnCode = object.getJsonObject("supervisoryNode").getJsonString(CODE);
+    JsonString searchedProgramCode = object.getJsonObject("program").getJsonString(CODE);
+    JsonString searchedFacilityCode = object.getJsonObject("supplyingFacility").getJsonString(CODE);
+
+    JsonArray array = findAll();
+    for (int i = 0; i < array.size(); i++) {
+      JsonObject next = array.getJsonObject(i);
+      JsonString foundSnCode = next.getJsonObject("supervisoryNode").getJsonString(CODE);
+      JsonString foundProgramCode = next.getJsonObject("program").getJsonString(CODE);
+      JsonString foundFacilityCode = next.getJsonObject("supplyingFacility").getJsonString(CODE);
+
+      if (searchedSnCode.equals(foundSnCode)
+          && searchedProgramCode.equals(foundProgramCode)
+          && searchedFacilityCode.equals(foundFacilityCode)) {
+        return next;
+      }
+    }
+    return null;
   }
 
 }
