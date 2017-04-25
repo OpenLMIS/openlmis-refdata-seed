c99ba381351ed4aaf4dfedcb8905bc1e0760421c
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2017-04-25 13:52:34 +0200

    Remove unnecessary code

--- src/main/java/mw/gov/health/lmis/upload/FacilityTypeApprovedProductService.java
+++ src/main/java/mw/gov/health/lmis/upload/FacilityTypeApprovedProductService.java
@@ -3,9 +3,7 @@ package mw.gov.health.lmis.upload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonString;
 
 @Service
 public class FacilityTypeApprovedProductService extends BaseCommunicationService {
@@ -25,23 +23,6 @@ public class FacilityTypeApprovedProductService extends BaseCommunicationService
 
   @Override
   public JsonObject findUnique(JsonObject object) {
-    JsonString searchedFacilityTypeCode = object.getJsonObject("facilityType").getJsonString(CODE);
-    JsonString searchedProgramCode = object.getJsonObject("program").getJsonString(CODE);
-    JsonString searchedProductCode = object.getJsonObject("orderable").getJsonString("productCode");
-
-    JsonArray array = findAll();
-    for (int i = 0; i < array.size(); i++) {
-      JsonObject next = array.getJsonObject(i);
-      JsonString foundFacilityTypeCode = next.getJsonObject("facilityType").getJsonString(CODE);
-      JsonString foundProgramCode = next.getJsonObject("program").getJsonString(CODE);
-      JsonString foundProductCode = next.getJsonObject("orderable").getJsonString("productCode");
-
-      if (searchedFacilityTypeCode.equals(foundFacilityTypeCode)
-          && searchedProgramCode.equals(foundProgramCode)
-          && searchedProductCode.equals(foundProductCode)) {
-        return next;
-      }
-    }
     return null;
   }
 }
