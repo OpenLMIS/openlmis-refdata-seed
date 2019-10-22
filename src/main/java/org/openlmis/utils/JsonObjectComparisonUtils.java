ce5fc7c3c878704cf0a0470acd1a29a05d016061
Daniel Serkowski <dserkowski@soldevelo.com>
2019-10-22 18:39:39 +0200

    ANGOLASUP-113: Fixed issues with comparing numeric values

--- src/main/java/org/openlmis/utils/JsonObjectComparisonUtils.java
+++ src/main/java/org/openlmis/utils/JsonObjectComparisonUtils.java
@@ -1,9 +1,12 @@
 package org.openlmis.utils;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
+import javax.json.JsonValue.ValueType;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.openlmis.DataSeeder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +14,8 @@ import org.slf4j.LoggerFactory;
 public class JsonObjectComparisonUtils {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DataSeeder.class);
+
+  private static final int FLOAT_PRECISION = 5;
 
   /**
    * Compares two JSONs: based on input entry (CSV) and existing object fetched form OLMIS. The
@@ -26,8 +31,8 @@ public class JsonObjectComparisonUtils {
       JsonValue existingValue = existingObject.getOrDefault(entry.getKey(), JsonValue.NULL);
       if (!jsonValuesEqual(entry.getValue(), existingValue)) {
         LOGGER.info(entry.getKey() + " has changed.");
-        LOGGER.debug("Previous value: " + existingValue.toString());
-        LOGGER.debug("New value: " + entry.getValue().toString());
+        LOGGER.debug("Previous value: " + getStringValue(existingValue));
+        LOGGER.debug("New value: " + getStringValue(entry.getValue()));
         LOGGER.debug("Skipping next checks. Note: there could be more differences.");
         return false;
       }
@@ -58,9 +63,29 @@ public class JsonObjectComparisonUtils {
     } else if (newValue.getValueType().equals(JsonValue.ValueType.OBJECT)
         && existingValue.getValueType().equals(JsonValue.ValueType.OBJECT)) {
       return equals((JsonObject) newValue, (JsonObject) existingValue);
+    } else if (existingValue.getValueType().equals(ValueType.NUMBER)) {
+      return isEqualNumerically(getStringValue(existingValue), getStringValue(newValue));
     }
-    return newValue.toString().replace("\"", "").equals(
-        existingValue.toString().replace("\"", ""));
+    return getStringValue(newValue).equals(getStringValue(existingValue));
+  }
+
+  private static boolean isEqualNumerically(String value1, String value2) {
+    boolean isExitingValueNumeric = NumberUtils.isParsable(value1);
+    boolean isNewValueNumeric = NumberUtils.isParsable(value2);
+
+    if (isExitingValueNumeric && isNewValueNumeric) {
+      return createBigDecimal(value1).compareTo(createBigDecimal(value2)) == 0;
+    }
+    return false;
+  }
+
+  private static BigDecimal createBigDecimal(String value) {
+    return NumberUtils.createBigDecimal(value)
+        .setScale(FLOAT_PRECISION, BigDecimal.ROUND_HALF_EVEN);
+  }
+
+  private static String getStringValue(JsonValue str) {
+    return str.toString().replace("\"", "");
   }
 
   private JsonObjectComparisonUtils() {
